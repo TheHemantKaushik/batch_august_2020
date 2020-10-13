8b9d2f2e610cda05985fe88fa97e308e748a4c8c
Hemant Kaushik <thehemantkaushik@gmail.com>
2020-10-13 15:05:42 +0530

    Login app added

--- _projects/03_login_app/lib/login_page.dart
+++ _projects/03_login_app/lib/login_page.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:login_app/forgot_password_page.dart';
+import 'package:login_app/utils/form_validators.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   final _formController = GlobalKey<FormState>();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
+  bool _autoValidate = false;
+
   void _login() {
     print('Login pressed');
 
-    final email = _emailController.text;
-    final password = _passwordController.text;
+    if (_autoValidate == false) {
+      setState(() {
+        _autoValidate = true;
+      });
+    }
 
-    print('email=$email, password=$password');
+    bool isFormValid = _formController.currentState.validate();
+    print('isFormValid=$isFormValid');
+
+    if (isFormValid) {
+      _formController.currentState.save();
+
+      final email = _emailController.text.trim();
+      final password = _passwordController.text.trim();
+
+      print('email=$email, password=$password');
+
+      // setState(() {
+      //   _emailController.text = "email@gmail.com";
+      // });
+    }
   }
 
   @override
@@ -28,18 +53,23 @@ class LoginPage extends StatelessWidget {
           key: _formController,
           child: Column(
             children: [
-              Flexible(child: Container()),
+              // Flexible(child: Container()),
+              SizedBox(height: 100),
               TextFormField(
+                autovalidate: _autoValidate,
                 controller: _emailController,
                 decoration: InputDecoration(
                   hintText: "Email",
                 ),
+                validator: validateEmail,
               ),
               TextFormField(
+                autovalidate: _autoValidate,
                 controller: _passwordController,
                 decoration: InputDecoration(
                   hintText: "Password",
                 ),
+                validator: validatePassword,
               ),
               Align(
                 alignment: Alignment.centerRight,
@@ -63,7 +93,7 @@ class LoginPage extends StatelessWidget {
                   textColor: Colors.white,
                 ),
               ),
-              Flexible(child: Container()),
+              // Flexible(child: Container()),
               Text(
                 "Or login with:",
                 style: Theme.of(context).textTheme.caption,
